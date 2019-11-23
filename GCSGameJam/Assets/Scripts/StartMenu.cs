d12ae4ea6f0deeffe498e41ed7c12b652fb0eea0
nnnathannnnn <33075001+nnnathannnnn@users.noreply.github.com>
2019-11-23 12:14:11 -0700

    TextMeshPro, Added Menu Scene

---
+++ GCSGameJam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject LoadingScreen = null;
+
+    public void LoadScene(string sceneName)
+    {
+        if (!sceneName.Equals(""))
+        {
+            if (LoadingScreen != null)
+                LoadingScreen.SetActive(true);
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
